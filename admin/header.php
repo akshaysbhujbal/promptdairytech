d9c150d925c2bc59e9eee64425d06888c450cfda
Akshay Bhujbal <akshaysbhujbal90@gmail.com>
2024-09-23 12:01:12 +0530

    created header file
    created by Akshay Bhujbal on 23rd Sept 2024

---
+++ admin/header.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="dashboard.php">Prompt Dairy Tech</a>
+            </div>
+
+            <!--div class="header-right">
+
+                <a href="message-task.html" class="btn btn-info" title="New Message"><b>30 </b><i class="fa fa-envelope-o fa-2x"></i></a>
+                <a href="message-task.html" class="btn btn-primary" title="New Task"><b>40 </b><i class="fa fa-bars fa-2x"></i></a>
+                <a href="login.html" class="btn btn-danger" title="Logout"><i class="fa fa-exclamation-circle fa-2x"></i></a>
+
+            </div-->
+        </nav>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                    
+                    <li>
+                        <a class="active-menu" href="dashboard.php"><i class="fa fa-dashboard "></i>Prompt</a>
+                    </li>
+                   
+                    <li>
+                        <a href="category.php"><i class="fa fa-pie-chart "></i>Categories</a>
+                        
+                    </li>
+                    <li>
+                        <a href="items.php"><i class="fa fa-tasks"></i>Item List</a>
+                        
+                    </li>
+                    <li>
+                        <a href="newbill.php"><i class="fa fa-plus "></i>New Bills</a>
+                        
+                    </li>
+                    
+                    <li>
+                        <a href="billhistory.php"><i class="fa fa-history"></i>Bill History</a>
+                    </li>
+
+                    <li>
+                        <a href="report.php"><i class="fa fa-print"></i>Reports</a>
+                    </li>
+                     
+                </ul>
+
+            </div>
+
+        </nav>
