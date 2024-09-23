976c880debba0a0429bc9a934ed64bed26c5d11d
Akshay Bhujbal <akshaysbhujbal90@gmail.com>
2024-09-23 11:43:51 +0530

    navigation bar file created
    created by Akshay Bhujbal on 23rd Sept 2024

---
+++ nav.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Prompt Dairy Tech</title>
+  <link rel="shortcut icon" type="x-icon" href="../images/ekomilkultramb.jpg">
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+  <section id="topbar" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">promptdairytech@gmail.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><a href="#contact"><span>+911234567891</span></a></i>
+      </div>
+      <div class="social-links d-none d-md-flex align-items-center">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+      </div>
+    </div>
+  </section>
+
+  <header id="header" class=" align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+      <img style="height: 70px; width: 70px;" src="images/prompt.jpeg">
+      <h1 class="logo" style="font-family: Engravers MT;font-size: 23px; margin-left:-200px; line-height:20px; "><a href="index.html">Prompt Dairy Tech<span></span></a></h1>
+      
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li class=""><a href="admin/login.php"><span>Login</span> <!-- <i class="bi bi-chevron-down"></i> --></a>
+            <ul>
+              <!-- <li><a href="login_form.php">Admin Login</a></li> -->
+              <!-- <li><a href="admin/login.php">Branch Login</a></li> -->
+              
+            </ul>
+          </li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </header>
+
+</body>
+
+</html>
+
+
